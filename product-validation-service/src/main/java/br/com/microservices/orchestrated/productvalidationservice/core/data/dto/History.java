a4d1a21e55b8ca27efb3993b0970310bfca5674f
EnzoMendes34 <enzomendes150124@gmail.com>
2026-04-09 16:33:24 -0300

    first commit

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/data/dto/History.java
@@ -0,0 +1,20 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class History {
+
+    private String source;
+    private String status;
+    private String message;
+    private LocalDateTime createdAt;
+}
